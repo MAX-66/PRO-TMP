5af3c94240264c8651afa5db43b99a50236e5208
lxq <luoxinqiong@pinlor.com>
2024-01-06 18:16:41 +0800

    feat: spring doc
    1、使用spring doc 作为api基础，放弃使用全套 knife4j
    2、修改knife4j公用依赖包为 doc
    3、gateway 文档模块完成

---
+++ brenden-cloud-gateway/src/main/java/com/brenden/cloud/config/SpringDocConfiguration.java
@@ -0,0 +1,52 @@
+package com.brenden.cloud.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author lxq
+ * @since 2024/1/3
+ */
+@Configuration
+@RequiredArgsConstructor
+public class SpringDocConfiguration {
+
+    private final static String API_URI = "/v3/api-docs";
+
+    private final static String API_NAME_PREFIX = "api-";
+
+
+    protected final SwaggerUiConfigProperties swaggerUiConfigProperties;
+
+    protected final RouteDefinitionLocator routeDefinitionLocator;
+
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = routeDefinitionLocator.getRouteDefinitions().map(routeDefinition -> {
+            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + routeDefinition.getId() + "/**").group(routeDefinition.getId()).build());
+            return new AbstractSwaggerUiConfigProperties.SwaggerUrl(
+                    routeDefinition.getId(),
+                    routeDefinition.getId() + API_URI,
+                    API_NAME_PREFIX.concat(routeDefinition.getId())
+            );
+        }).collect(Collectors.toSet()).block();
+        swaggerUiConfigProperties.setUrls(urls);
+        return groups;
+    }
+
+}
